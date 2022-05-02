3b14856f31524b3083ed9d83d73f0de10c06a135
wizardofwaz8282 <104391698+wizardofwaz8282@users.noreply.github.com>
2022-05-01 19:34:02 -0500

    Added Fifth Assginment
    Fifth assignment add and edited index

---
+++ fifthAsg/FourthBug/buggy4.js
@@ -0,0 +1,13 @@
+function somethingElse() {
+    if (5 > 2) {
+        alert("Bigger");
+    // Bug 1: Added } to close if statement above
+    }
+    if (5 < 2) {
+        alert("Smaller");
+    }
+}
+
+function gogogo() {
+    alert("Yay, it works!");
+}
---
+++ fifthAsg/ThirdBug/buggy3.js
@@ -0,0 +1,7 @@
+// Bug 1: Removed ; after function name
+function gogogo() {
+    // Bug 2: Alert was misspelled
+    // Bug 3: Alert message was not properly closed (used '' instead of ")
+    alert("Yay, it works!");
+    // Bug 4: Used regular brackets ) instead of curly bracket } to close function gogogo() 
+}
