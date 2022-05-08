2bbe2ca104e37c7c8a169ccd07d8255aa3913095
wizardofwaz8282 <104391698+wizardofwaz8282@users.noreply.github.com>
2022-05-07 23:23:29 -0500

    bankaccount+index update

--- fifthAsg/FirstBug/buggy1.js
+++ fifthAsg/FirstBug/buggy1.js
@@ -1,3 +1,7 @@
 function gogogo() {
-    alert("Yay, it works!");
+    var pVal = document.getElementById(".thiss");
+    pVal.style.display=none;
+}
+function gogogo2(){
+    return function(){alert("YEA BOIIII!")}
 }
